16d42b1c497c5393ceb7d475f18ecfe921150167
Jisu Choi <redgem92@gmail.com>
2022-08-24 23:28:46 +0900

    Initial project

---
+++ src/test/java/dev/everyhinglikeanoob/personalblog/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.everyhinglikeanoob.personalblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
